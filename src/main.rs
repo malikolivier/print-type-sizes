f2be7ca8540fb2017aeb6171b3a03ca914965b6e
Malik Olivier Boussejra <malik@boussejra.com>
2019-12-06 16:28:10 +0900

    Print type sizes

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use cargo::ops;
 use cargo::util::config::Config;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
+    let rust_flags = if let Ok(flags) = env::var("RUSTFLAGS") {
+        flags
+    } else {
+        String::new()
+    };
+    env::set_var("RUSTFLAGS", format!("{} -Z print-type-sizes", rust_flags));
     let config = Config::default()?;
     let options = ops::CompileOptions::new(&config, CompileMode::Check { test: false })?;
     let path = env::current_dir()?.join("Cargo.toml");
