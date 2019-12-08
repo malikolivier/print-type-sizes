9bfbb62c0330a43a841894286577f095c96ab233
Malik Olivier Boussejra <malik@boussejra.com>
2019-12-09 07:54:35 +0900

    Print types ordered!

--- src/main.rs
+++ src/main.rs
@@ -28,20 +28,62 @@ fn compile() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-fn parse_output<R: Read>(stdout: R) {
+struct Type {
+    name: String,
+    size: usize,
+}
+
+fn parse_output<R: Read>(stdout: R) -> Vec<Type> {
+    const PRINT_TYPE_SIZE: &str = "print-type-size ";
+    const PRINT_TYPE_SIZE_TYPE: &str = "print-type-size type: ";
+    use std::cmp::Ordering;
+
+    let mut types = Vec::new();
+
     let f = io::BufReader::new(stdout);
     for line in f.lines() {
-        eprintln!("{:?}", line);
+        if let Ok(line) = line {
+            if line.starts_with(PRINT_TYPE_SIZE) {
+                if line.starts_with(PRINT_TYPE_SIZE_TYPE) {
+                    let mut split = line.split("`");
+                    if let Some(type_name) = split.nth(1) {
+                        if let Some(metadata) = split.next() {
+                            let mut space_split = metadata.split(" ");
+                            space_split.next();
+                            if let Some(size_str) = space_split.next() {
+                                if let Ok(size) = size_str.parse::<usize>() {
+                                    types.push(Type {
+                                        name: type_name.to_owned(),
+                                        size,
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    types.sort_by(|t1, t2| match t2.size.cmp(&t1.size) {
+        Ordering::Equal => t1.name.cmp(&t2.name),
+        ord => ord,
+    });
+
+    types
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     set_rust_flags();
 
-    {
+    let types = {
         let mut stdout = gag::BufferRedirect::stdout()?;
         compile()?;
-        parse_output(&mut stdout);
+        parse_output(&mut stdout)
+    };
+
+    for t in types {
+        println!("{}\t{}", t.name, t.size);
     }
 
     Ok(())
